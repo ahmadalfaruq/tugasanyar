4f697180be3050773daffeb8f364687e690cd6bd
ahmadalfaruq <ahmadalfaruq33@gmail.com>
2019-12-20 21:32:35 +0700

    tugaspayanyan

---
+++ 5-Organisasi Program dengan Package/src/Program_manual/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_manual;
+
+/**
+ * Nama : ahmad al faruq
+ * NIM  : A2.1900010
+ * Kelas: TI-1C
+ */
+public class Main {
+    public static void main(String[] args){
+		System.out.print("nama ku adalah Dani, teman si fajar");
+	}
+}
---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/*
+ * Nama : ahmad al faruq
+ * NIM  : A2.1900010
+ * Kelas: TI-1C
+ *
+ * @author SonySA
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
